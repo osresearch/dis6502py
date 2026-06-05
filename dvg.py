83ae57807c845afffd55fb704f276a02a3ceb526
Trammell Hudson <hudson@trmm.net>
2026-06-05 18:20:38 +0200

    Digital Vector Generator emulator

---
+++ dvg.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3 
+# Emulate the Atari Digital Vector Generator
+# and output an SVG rendering of a command stream
+# https://computerarcheology.com/Arcade/Asteroids/DVG.html
+import drawsvg as draw
+import struct
+import sys
+from hashlib import sha256
+
+class DVG:
+	def __init__(self, rom):
+		# ROM starts at 0x800
+		self.rom = rom
+		self.debug = False
+		self.reset()
+
+	def reset(self):
+		self.pc = 0
+		self.x = 0
+		self.y = 0
+		self.s = 3
+		self.stack = []
+
+	def trace(self, *s):
+		if self.debug:
+			print(*s, file=sys.stderr)
+
+	def add_line(self, bright, scale, sx, x, sy, y):
+		if sx: x = -x
+		if sy: y = -y
+
+		scale = 2 ** (10 - self.s - scale)
+		nx = self.x + x / scale
+		ny = self.y - y / scale
+
+		if bright != 0:
+			self.d.append(draw.Lines(
+				self.x, self.y, nx, ny,
+				fill='none',
+				stroke_width=1,
+				stroke='#%01x%01x%01x' % (bright,bright,bright),
+			))
+		else:
+			self.d.append(draw.Circle(nx, ny, 1, fill='#888'))
+
+		self.x = nx
+		self.y = ny
+
+	def read16(self,addr):
+		if addr < 0x800:
+			if addr > len(self.ram) - 1:
+				return None
+			(word,) = struct.unpack("<H", self.ram[addr:addr+2])
+		else:
+			addr -= 0x800
+			if addr > len(self.rom) - 1:
+				return None
+			(word,) = struct.unpack("<H", self.rom[addr:addr+2])
+		return word
+
+	def process(self, ram):
+		self.d = draw.Drawing(
+			1024, 1024,
+			origin=(0,0),
+			style="background-color:black",
+			id_prefix=sha256(ram).hexdigest()[0:8],
+		)
+		self.ram = ram
+		while self.execute(self.pc):
+			pass
+		return self.d.as_svg()
+
+	def execute(self, cmd):
+		cmd = self.read16(self.pc)
+		if not cmd:
+			return False
+		self.pc += 2
+
+		c = (cmd >> 12) & 0xF
+		if c <= 0x9:
+			# VEC
+			cmd2 = self.read16(self.pc)
+			self.pc += 2
+			scale = (cmd >> 12) & 0xF
+			sy = (cmd >> 10) & 1
+			y = (cmd >> 0) & 0x3FF
+			bright = (cmd2 >> 12) & 0xF
+			sx = (cmd2 >> 10) & 1
+			x = (cmd2 >> 0) & 0x3FF
+			self.trace("%04x%04x" % (cmd, cmd2), f"{scale=} {sx=} {x=} {bright=} {sy=} {y=}")
+			self.add_line(bright, scale, sx, x, sy, y)
+		elif c == 0xA:
+			# LABS
+			# this sets the global scale factor and moves ox,oy
+			cmd2 = self.read16(self.pc)
+			self.pc += 2
+			self.y = (cmd >> 0) & 0x3FF
+			self.x = (cmd2 >> 0) & 0x3FF
+			self.s = (cmd2 >> 12) & 0xF
+			self.trace(f"LABS scale={self.s} x={self.x} y={self.y}")
+		elif c == 0xB:
+			# HALT
+			self.trace("HALT")
+			return False
+		elif c == 0xC:
+			# JSR
+			self.stack.append(self.pc)
+			self.pc = ((cmd & 0xFFF) << 1)
+			self.trace("JSR %04x (%04x)" % (self.pc, cmd))
+		elif c == 0xD:
+			# RTS
+			if len(self.stack) == 0:
+				self.trace("RTS empty")
+				return False
+			self.pc = self.stack[-1]
+			self.stack = self.stack[0:-1]
+			self.trace("RTS %04x" %(self.pc))
+		elif c == 0xE:
+			# JMP
+			self.pc = ((cmd & 0xFFF) << 1)
+			self.trace("JMP %04x (%04x)" % (self.pc, cmd))
+		elif c == 0xF:
+			# SVEC
+			s1 = (cmd >> 11) & 0x1
+			sy = (cmd >> 10) & 0x1
+			y  = (cmd >>  8) & 0x3
+			b  = (cmd >>  4) & 0xF
+			s2 = (cmd >>  3) & 0x1
+			sx = (cmd >>  2) & 0x1
+			x  = (cmd >>  0) & 0x3
+
+			scale = (s2 << 1 | s1)
+			self.trace("%04x" % (cmd), f"scale={s2}{s1}={scale} {sx=} {x=} {b=} {sy=} {y=}")
+			self.add_line(b, 2**scale, sx, x, sy, y)
+
+		return True
+		
+#d.save_svg("output.svg")
+
+
+if __name__ == "__main__":
+	rom = open("llander/llander-0x4800.bin", "rb").read()
+	rom = rom[:0x6000-0x4800]
+
+	dvg = DVG(rom)
+
+	font_addr = 0x57a2 - 0x4800
+	font = rom[font_addr:font_addr+32]
+
+	dvg.s = 12
+	dvg.x = 512
+	dvg.y = 512
+	print(dvg.process(font))
